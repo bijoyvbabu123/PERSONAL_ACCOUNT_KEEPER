e55f2af54f38b01ec7b3a37ef0a739f9ca9a8b7a
bijoyvbabu123 <73835543+bijoyvbabu123@users.noreply.github.com>
2021-04-22 03:52:49 +0530

    Developed the initial module and the opening main menu window.

--- MAIN_MENU_WINDOW.py
+++ MAIN_MENU_WINDOW.py
@@ -1,1 +1,43 @@
 """This module provides navigation to various domains that will be available is this software"""
+
+import tkinter
+import os
+import HOME_ACC_MENU
+
+
+def main_function():
+    main_menu_root = tkinter.Tk()
+    main_menu_root.minsize(400, 400)
+    main_menu_root.resizable(False, False)
+    main_menu_root.title("PERSONAL ACCOUNT KEEPER")
+
+    def home_accounting():
+        main_menu_root.destroy()
+        HOME_ACC_MENU.main_function()
+        main_function()
+
+    home_accounting_icon = tkinter.PhotoImage(
+        file=os.path.join('FILES', 'HOME_ACCOUNTING', 'icons', 'home_accounting_menu.png'))
+    label_home_accounting = tkinter.Label(main_menu_root, text='HOME ACCOUNTING', font='TkDefaultFont 9 italic')
+    label_home_accounting.place(relx=0.120, rely=0.52)
+    label_home_accounting.bind("<Enter>",
+                               lambda event=None: label_home_accounting.config(font='TkDefaultFont 9 italic underline',
+                                                                               fg='DodgerBlue2'))
+    label_home_accounting.bind("<Leave>", lambda event=None: label_home_accounting.config(font='TkDefaultFont 9 italic',
+                                                                                          fg='SystemButtonText'))
+
+    label_coming = tkinter.Label(main_menu_root, text="** COMING", font='TkDefaultFont 13 italic')
+    label_soon = tkinter.Label(main_menu_root, text="SOON **", font='TkDefaultFont 13 italic')
+    label_coming.place(relx=0.6, rely=0.25)
+    label_soon.place(relx=0.62, rely=0.35)
+
+    button_home_accounting = tkinter.Button(main_menu_root, image=home_accounting_icon, command=home_accounting, relief='groove')
+    button_home_accounting.place(relx=0.1, rely=0.2)
+    button_home_accounting.bind("<Enter>",
+                                lambda event=None: label_home_accounting.config(font='TkDefaultFont 9 italic underline',
+                                                                                fg='DodgerBlue2'))
+    button_home_accounting.bind("<Leave>", lambda event=None: label_home_accounting.config(font='TkDefaultFont 9 italic',
+                                                                                           fg='SystemButtonText'))
+    label_home_accounting.bind("<Button-1>", lambda event=None: button_home_accounting.invoke())
+
+    main_menu_root.mainloop()
--- PERSONAL_ACCOUNT_KEEPER.py
+++ PERSONAL_ACCOUNT_KEEPER.py
@@ -1,1 +1,11 @@
 """This is the initial (main) module and this will be only for calling the main menu module"""
+
+import MAIN_MENU_WINDOW
+
+
+def main_menu():
+    MAIN_MENU_WINDOW.main_function()
+
+
+if __name__ == '__main__':
+    main_menu()
